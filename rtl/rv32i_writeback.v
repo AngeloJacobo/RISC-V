c61daf93e972f752440812ffe07a69203d537d83
Angelo Jacobo <jacobo.angeloECE@gmail.com>
2022-03-05 13:20:04 +0800

    Update rv32i_writeback.v

--- rtl/rv32i_writeback.v
+++ rtl/rv32i_writeback.v
@@ -11,45 +11,45 @@ module rv32i_writeback(
 	output reg wr_rd //write rd to the base reg if enabled
 );
 	localparam R_TYPE = 7'b011_0011, //instruction types and its opcode
-		       I_TYPE = 7'b001_0011,
-				 LOAD = 7'b000_0011,
-		        STORE = 7'b010_0011,
-			   BRANCH = 7'b110_0011,
-			      JAL = 7'b110_1111,
-			     JALR = 7'b110_0111,
-			      LUI = 7'b011_0111,
-			    AUIPC = 7'b001_0111,
-		       SYSTEM = 7'b111_0011,
-			    FENCE = 7'b000_1111;
+		   I_TYPE = 7'b001_0011,
+		     LOAD = 7'b000_0011,
+		    STORE = 7'b010_0011,
+		   BRANCH = 7'b110_0011,
+		      JAL = 7'b110_1111,
+		     JALR = 7'b110_0111,
+		      LUI = 7'b011_0111,
+		    AUIPC = 7'b001_0111,
+     	           SYSTEM = 7'b111_0011,
+		    FENCE = 7'b000_1111;
 			
 	reg[31:0] a,sum;
 
 	always @* begin
-		pc_new = pc + 32'd4;
+	pc_new = pc + 32'd4;
         rd = 0;    
         
 		case(opcode)
 			R_TYPE,I_TYPE: rd = alu_out;
-					 LOAD: rd = data_load;
-				   BRANCH: if(alu_out[0]) begin //branch iff value of ALU is 1(true)
-							a = pc;
-							pc_new = sum;
-						   end
-					  JAL: begin
-							rd = pc_new;
-							a = pc;
-							pc_new = sum;
-						   end 
-					 JALR: begin
-							rd = pc_new;
-							a = rs1;
-							pc_new = sum;
-						   end
-					  LUI: rd = imm;
-					AUIPC: begin
-							a = pc;
-							rd = sum;
-						   end
+				 LOAD: rd = data_load;
+			       BRANCH: if(alu_out[0]) begin //branch iff value of ALU is 1(true)
+						a = pc;
+						pc_new = sum;
+				       end
+				  JAL: begin
+						rd = pc_new;
+						a = pc;
+						pc_new = sum;
+				       end 
+				 JALR: begin
+						rd = pc_new;
+						a = rs1;
+						pc_new = sum;
+					end
+				  LUI: rd = imm;
+				AUIPC: begin
+						a = pc;
+						rd = sum;
+					   end
 		endcase
         
 		sum = a + imm; //share adder for all addition operation for less resource utilization	
