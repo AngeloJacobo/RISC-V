320fb3cb74789fc516683c66b7083da69dfee359
Angelo Jacobo <jacobo.angeloECE@gmail.com>
2022-03-05 13:17:51 +0800

    Update rv32i_soc.v

--- rtl/rv32i_soc.v
+++ rtl/rv32i_soc.v
@@ -1,12 +1,12 @@
 `timescale 1ns / 1ps
 
 module rv32i_soc #(parameter PC_RESET=32'h00_00_00_00, ROM_BYTES=1024, RAM_BYTES=1024) (
-    input clk,
-    input rst_n
+    input wire clk,
+    input wire rst_n
     );
     
-    //Instruction Memory Interface
-    wire[31:0] inst; 
+    	//Instruction Memory Interface
+    	wire[31:0] inst; 
 	wire[31:0] iaddr;  
 	//Data Memory Interface
 	wire[31:0] din; //data r
@@ -16,8 +16,8 @@ module rv32i_soc #(parameter PC_RESET=32'h00_00_00_00, ROM_BYTES=1024, RAM_BYTES
 	wire wr_en; //write enable 
 	
     rv32i_core #(.PC_RESET(32'h00_00_00_00)) m0( //main RV32I core
-	    .clk(clk),
-	    .rst_n(rst_n),
+	.clk(clk),
+	.rst_n(rst_n),
         //Instruction Memory Interface
         .inst(inst), //32-bit instruction
         .iaddr(iaddr), //address of instruction 
@@ -46,8 +46,6 @@ endmodule
 
 
 
-
-
 /******************** RAM and ROM modules for the Instruction and Data Memory********************/
 
 module inst_mem #(parameter ROM_DEPTH=1024) ( //ROM_DEPTH = number of BYTES (since this is byte-addressable)
@@ -56,11 +54,9 @@ module inst_mem #(parameter ROM_DEPTH=1024) ( //ROM_DEPTH = number of BYTES (sin
 );
     
     reg[31:0] regfile[ROM_DEPTH/4 - 1:0];
-
-    
     integer i;
-    initial begin //initialize instruction memory
-        for(i=0 ; i<ROM_DEPTH ; i=i+1) regfile[i]=0; //initialize all locations to zero first
+    initial begin //initialize instruction memory to zero
+        for(i=0 ; i<ROM_DEPTH ; i=i+1) regfile[i]=0; 
     end
     assign inst=regfile[addr[$clog2(ROM_DEPTH)-1:2]];
     
@@ -79,8 +75,8 @@ module data_mem #(parameter RAM_DEPTH=1024) ( //RAM_DEPTH = number of BYTES (sin
     
     
     integer i;
-    initial begin //initialize instruction memory
-        for(i=0 ; i<RAM_DEPTH ; i=i+1) regfile[i]=0; //initialize all locations to zero first
+    initial begin //initialize instruction memory o zero
+        for(i=0 ; i<RAM_DEPTH ; i=i+1) regfile[i]=0; 
     end
   
     always @* begin
