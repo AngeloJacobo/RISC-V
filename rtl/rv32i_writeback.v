2227ef5898feee9756f580efa4ab66f6bdfbb4fd
AngeloJacobo <jacobo.angeloece@gmail.com>
2022-03-14 20:02:05 +0800

    changed to single bit decoding

--- rtl/rv32i_writeback.v
+++ rtl/rv32i_writeback.v
@@ -3,53 +3,65 @@
 `timescale 1ns / 1ps
 
 module rv32i_writeback(
-    input wire[6:0] opcode, //instruction class type
+    input wire clk, rst_n,
     input wire[31:0] alu_out,//output of ALU
     input wire[31:0] pc, //current PC value
     input wire[31:0] imm, //immediate value
     input wire[31:0] rs1, //source register 1 value
     input wire[31:0] data_load, //data to be loaded to base reg
+    input wire opcode_rtype,
+    input wire opcode_itype,
+    input wire opcode_load,
+    input wire opcode_store,
+    input wire opcode_branch,
+    input wire opcode_jal,
+    input wire opcode_jalr,
+    input wire opcode_lui,
+    input wire opcode_auipc,
+    input wire opcode_system,
+    input wire opcode_fence,  
     output reg[31:0] rd, //value to be written back to destination register
     output reg[31:0] pc_new, //new PC value
     output reg wr_rd //write rd to the base reg if enabled
 );
-    localparam R_TYPE = 7'b011_0011, //instruction types and its opcode
-               I_TYPE = 7'b001_0011,
-                 LOAD = 7'b000_0011,
-                STORE = 7'b010_0011,
-               BRANCH = 7'b110_0011,
-                  JAL = 7'b110_1111,
-                 JALR = 7'b110_0111,
-                  LUI = 7'b011_0111,
-                AUIPC = 7'b001_0111,
-               SYSTEM = 7'b111_0011,
-                FENCE = 7'b000_1111;
-            
+    reg[31:0] rd_d;
+    reg[31:0] pc_new_d;
+    reg wr_rd_d;
     reg[31:0] a,sum;
 
+    // register outputs of this module for shorter combinational timing paths
+    always @(posedge clk,negedge rst_n) begin
+        if(!rst_n) begin
+            rd <= 0; 
+            pc_new <= 0;
+            wr_rd <= 0;
+        end
+        else begin
+            rd <= rd_d;
+            pc_new <= pc_new_d;
+            wr_rd <= wr_rd_d;
+        end
+    end
+
+    //determine next value of PC and rd
     always @* begin
-    pc_new = pc + 32'd4;
-    rd = 0;
-    a = pc;
-    sum = a + imm; //share adder for all addition operation for less resource utilization   
+        rd_d = 0;
+        pc_new_d = pc + 32'd4;
+        wr_rd_d = 0;
+        a = pc;
+        sum = a + imm; //share adder for all addition operation for less resource utilization   
           
-        case(opcode)
-        R_TYPE,I_TYPE: rd = alu_out;
-                 LOAD: rd = data_load;
-               BRANCH: if(alu_out[0]) pc_new = sum; //branch iff value of ALU is 1(true)
-                  JAL: begin
-                        rd = pc_new;
-                        pc_new = sum;
-                       end 
-                 JALR: begin
-                        rd = pc_new;
-                        a = rs1;
-                        pc_new = sum;
-                       end
-                  LUI: rd = imm;
-                AUIPC: rd = sum;
-        endcase
-        if(opcode == BRANCH || opcode == STORE || opcode == SYSTEM) wr_rd = 0;
-        else wr_rd = 1; //always write to the destination reg except when instruction is BRANCH or STORE or SYSTEM
+        if(opcode_rtype || opcode_itype) rd_d = alu_out;
+        if(opcode_load) rd_d = data_load;
+        if(opcode_branch && alu_out[0]) pc_new_d = sum; //branch iff value of ALU is 1(true)
+        if(opcode_jal || opcode_jalr) begin
+            rd_d = pc_new_d;
+            pc_new_d = sum;
+        end 
+        if(opcode_jalr) a = rs1;
+        if(opcode_lui) rd_d = imm;
+        if(opcode_auipc) rd_d = sum;
+        if(opcode_branch || opcode_store || opcode_system) wr_rd_d = 0;
+        else wr_rd_d = 1; //always write to the destination reg except when instruction is BRANCH or STORE or SYSTEM
     end
 endmodule
