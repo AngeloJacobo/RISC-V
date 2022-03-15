bac99477704580f162601bd6baac9fe829082a04
AngeloJacobo <jacobo.angeloece@gmail.com>
2022-03-15 16:06:09 +0800

    changed to single bit decoding

--- rtl/rv32i_alu.v
+++ rtl/rv32i_alu.v
@@ -3,52 +3,59 @@
 `timescale 1ns / 1ps
 
 module rv32i_alu(
+    input clk,rst_n,
     input wire[31:0] a, //rs1 or pc
     input wire[31:0] b, //rs2 or imm 
-    input wire[3:0] op, //operation
-    output reg[31:0] y //result of arithmetic operation
+    output reg[31:0] y, //result of arithmetic operation
+    /// ALU Operations ///
+    input wire alu_add, //addition
+    input wire alu_sub, //subtraction
+    input wire alu_slt, //set if less than
+    input wire alu_sltu, //set if less than unsigned 
+    input wire alu_xor, //bitwise xor
+    input wire alu_or, //bitwise or
+    input wire alu_and, //bitwise and
+    input wire alu_sll, //shift left logical
+    input wire alu_srl, //shift right logical
+    input wire alu_sra, //shift right arithmetic
+    input wire alu_eq, //equal
+    input wire alu_neq, //not equal
+    input wire alu_ge, //greater than or equal
+    input wire alu_geu //greater than or equal unisgned
 );
-    //operations supported by this ALU
-    localparam  ADD = 0, //addition
-                SUB = 1, //subtraction
-                SLT = 2, //set if less than
-               SLTU = 3, //set if less than unsigned
-                XOR = 4, //bitwise xor
-                 OR = 5, //bitwise or
-                AND = 6, //bitwise and
-                SLL = 7, //shift left logical
-                SRL = 8, //shift right logical
-                SRA = 9, //shift right arithmetic
-                 EQ = 10, //equal
-                NEQ = 11, //not equal
-                 GE = 12, //greater than or equal
-                GEU = 13; //greater than or equal unisgned
     
-    //combinational logic for alu   
-    always @* begin
-        case(op)
-            ADD: y = a + b;
-            SUB: y = a - b;
-       SLT,SLTU: begin
-                  y = a < b;
-                  if(op == SLT) y = (a[31] ^ b[31]) ? a[31]:y;
-                 end
-            XOR: y = a ^ b;
-             OR: y = a | b;
-            AND: y = a & b;
-            SLL: y = a << b[4:0];  
-            SRL: y = a >> b[4:0];
-            SRA: y = a >>> b[4:0];
-         EQ,NEQ: begin
-                  y = a == b;
-                  if(op == NEQ) y = !y;
-                 end
-         GE,GEU: begin
-                  y =  a >= b;
-                  if(op == GE) y = (a[31] ^ b[31]) ? b[31]:y;
-                 end 
-        default: y = 0;
-        endcase
+    reg[31:0] y_d;
+    
+    //register the output of ALU
+    always @(posedge clk, negedge rst_n) begin
+        if(!rst_n) y <= 0;
+        else y <= y_d;
+    end 
+
+    //ALU core (run in parallel instead of priority logic for less
+    always @* begin  //resource utilization)
+        y_d = 0;
+
+        if(alu_add) y_d = a + b;
+        if(alu_sub) y_d = a - b;
+        if(alu_slt || alu_sltu) begin
+            y_d = a < b;
+            if(alu_slt) y_d = (a[31] ^ b[31])? a[31]:y_d;
+        end 
+        if(alu_xor) y_d = a ^ b;
+        if(alu_or)  y_d = a | b;
+        if(alu_and) y_d = a & b;
+        if(alu_sll) y_d = a << b[4:0];
+        if(alu_srl) y_d = a >> b[4:0];
+        if(alu_sra) y_d = a >>> b[4:0];
+        if(alu_eq || alu_neq) begin
+            y_d = a == b;
+            if(alu_neq) y_d = !y_d;
+        end
+        if(alu_ge || alu_geu) begin
+            y_d = a >= b;
+            if(alu_ge) y_d = (a[31] ^ b[31])? b[31]:y_d;
+        end
     end
     
 endmodule
