b34012a1ff3d46724c127907ff39bdb7f6b79b16
Angelo Jacobo <jacobo.angeloECE@gmail.com>
2022-04-18 21:46:49 +0800

    initialize all basereg to zero to avoid error in testbench

--- rtl/rv32i_basereg.v
+++ rtl/rv32i_basereg.v
@@ -18,6 +18,10 @@ module rv32i_basereg
     reg[4:0] rs1_addr_q = 0,rs2_addr_q = 0;
     reg[31:0] base_regfile[31:1]; //base register file (base_regfile[0] is hardwired to zero)
         
+    initial begin //initialize instruction memory to zero
+        for(i=0 ; i<32 ; i=i+1) base_regfile[i]=0; 
+    end
+    
     always @(posedge clk) begin
         if(wr && rd_addr!=0) begin
            base_regfile[rd_addr] <= rd; //synchronous write
