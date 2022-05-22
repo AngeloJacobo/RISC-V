f4db32b3c103804944d1ab9b296096ac9047b7d6
Angelo Jacobo <jacobo.angeloECE@gmail.com>
2022-05-22 23:53:34 +0800

    fixed local to relative addressing

--- extra/lhu.s
+++ extra/lhu.s
@@ -16,7 +16,8 @@ _start: .global _start
 main:
         ### TEST CODE STARTS HERE ###
         # load halfword unsigned with zero imm
-        li  x1, 0x1008          # set x1 to 0x1008 
+        lla  x1, data            # set x1 to data
+        addi x1,x1, 8           # set x1 to data + 8 
         lhu  x2, 0(x1)          # load halfword from address 0x1008 to x2, x2=0x00005577
         
         # load halfword unsigned with positive imm
