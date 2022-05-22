97c3fea12590be2838fc49a6f0707c9cbb0d49a8
Angelo Jacobo <jacobo.angeloECE@gmail.com>
2022-05-22 18:23:21 +0800

    add trap-handler

--- rtl/rv32i_writeback.v
+++ rtl/rv32i_writeback.v
@@ -1,14 +1,22 @@
 //logic controller for the next PC and rd value [WRITEBACK STAGE]
 
 `timescale 1ns / 1ps
+`default_nettype none
 
 module rv32i_writeback #(parameter PC_RESET = 32'h00_00_00_00) (
     input wire clk, rst_n,
     input wire writeback, //enable wr_rd iff stage is currently on WRITEBACK
+    input wire[2:0] funct3, //function type 
     input wire[31:0] alu_out,//output of ALU
     input wire[31:0] imm, //immediate value
     input wire[31:0] rs1, //source register 1 value
     input wire[31:0] data_load, //data to be loaded to base reg
+    input wire[31:0] csr_out, //CSR value to be loaded to basereg
+    // Trap-Handler
+    input wire go_to_trap, //high before going to trap (if exception/interrupt detected)
+    input wire return_from_trap, //high before returning from trap (via mret)
+    input wire[31:0] return_address, //mepc CSR
+    input wire[31:0] trap_address, //mtvec CSR
     //// Opcode Type ////
     input wire opcode_rtype,
     input wire opcode_itype,
@@ -54,22 +62,33 @@ module rv32i_writeback #(parameter PC_RESET = 32'h00_00_00_00) (
         pc_d = pc + 32'd4;
         wr_rd_d = 0;
         a = pc;
-          
-        if(opcode_rtype || opcode_itype) rd_d = alu_out;
-        if(opcode_load) rd_d = data_load;
-        if(opcode_branch && alu_out[0]) pc_d = sum; //branch iff value of ALU is 1(true)
-        if(opcode_jal || opcode_jalr) begin
-            rd_d = pc_d;
-            pc_d = sum;
+
+        if(go_to_trap) pc_d = trap_address;  //interrupt or exception detected
+        
+        else if(return_from_trap) pc_d = return_address; //return from trap via mret
+        
+        else begin //normal operation
+            if(opcode_rtype || opcode_itype) rd_d = alu_out;
+            if(opcode_load) rd_d = data_load;
+            if(opcode_branch && alu_out[0]) pc_d = sum; //branch iff value of ALU is 1(true)
+            if(opcode_jal || opcode_jalr) begin
+                rd_d = pc_d;
+                pc_d = sum;
+                if(opcode_jalr) a = rs1;
+            end 
             if(opcode_jalr) a = rs1;
-        end 
-        if(opcode_jalr) a = rs1;
-        if(opcode_lui) rd_d = imm;
-        if(opcode_auipc) rd_d = sum;
-        if(opcode_branch || opcode_store || opcode_system) wr_rd_d = 0;
-        else wr_rd_d = 1; //always write to the destination reg except when instruction is BRANCH or STORE or SYSTEM  
+            if(opcode_lui) rd_d = imm;
+            if(opcode_auipc) rd_d = sum;
+            if(opcode_system && funct3!=0) begin //CSR write
+                rd_d = csr_out; 
+            end
+            
+            if(opcode_branch || opcode_store || (opcode_system && funct3 == 0) || opcode_fence ) wr_rd_d = 0; //funct3==0 are the non-csr system instructions
+            else wr_rd_d = 1; //always write to the destination reg except when instruction is BRANCH or STORE or SYSTEM(except CSR system instruction)  
+        end
         
     end
     
     assign sum = a + imm; //share adder for all addition operation for less resource utilization
+    
 endmodule
