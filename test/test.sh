ec55d07c4ca2bc0e0ba914fe1bfbf56ad7b07665
Angelo Jacobo <jacobo.angeloECE@gmail.com>
2022-06-18 16:54:05 +0800

    removed fsm and added fetch module

--- test/test.sh
+++ test/test.sh
@@ -47,11 +47,11 @@ fi
 # verilog rtl files of the RISC-V core
 rtlfiles="../rtl/rv32i_alu.v 
           ../rtl/rv32i_basereg.v 
+          ../rtl/rv32i_fetch.v
           ../rtl/rv32i_decoder.v 
           ../rtl/rv32i_memoryaccess.v 
           ../rtl/rv32i_writeback.v
           ../rtl/rv32i_csr.v
-          ../rtl/rv32i_fsm.v
           ../rtl/rv32i_core.v
           ../rtl/rv32i_soc.v
           ./rv32i_soc_TB.v"
