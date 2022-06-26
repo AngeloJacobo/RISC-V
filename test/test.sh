076bf7bd5433c4ea1415bccefe661f7ea83b816b
AngeloJacobo <jacobo.angeloece@gmail.com>
2022-06-26 09:58:06 +0800

    added script compatibility for both Modelsim and Icarus Verilog

--- test/test.sh
+++ test/test.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 # Configurables
- INDIVIDUAL_TESTDIR=./riscv-tests/isa/rv32ui  # directory of RISCV testcases used in debug mode (INDIVIDUAL TESTING)
-#INDIVIDUAL_TESTDIR=./extra  # directory of RISCV testcases used in debug mode (INDIVIDUAL TESTING)
+#INDIVIDUAL_TESTDIR=./riscv-tests/isa/rv32mi  # directory of RISCV testcases used in debug mode (INDIVIDUAL TESTING)
+INDIVIDUAL_TESTDIR=./extra  # directory of RISCV testcases used in debug mode (INDIVIDUAL TESTING)
 
 # Compilation parameters for RISC-V toolchain
 PREFIX=riscv64-unknown-elf-
 ONAME=test          # executable file name (xxxx.bin)
-MEMORY=memory.mem   # memory file name (extracted text and data sections from the executable file)
+MEMORY=memory.mem   # memory file name (extracted text and data sections from the executable file),filename used here must also be added to rv32i_soc_TB parameter for assurance
 #Note: Starting address of TEXT section is always zero. Starting address of DATA section depends on the testfile assembly
 
 
@@ -69,13 +69,18 @@ missinglist=""  # stores list of testfiles that are missing
 
 if [ "$1" == "compile" ] # compile-only the rtl files 
 then
-    if [ -d "./work/" ]  # check first if work library exists
-    then
-        vdel -all -lib work # delete old library folder
+    if [ $(command -v vlog) ] 
+    then                
+        if [ -d "./work/" ]  # check first if work library exists
+        then
+            vdel -all -lib work # delete old library folder
+        fi
+        vlib work
+        vlog ${rtlfiles}
+    else 
+        rm -f testbench.vvp # remove previous occurence of vvp file  
+        iverilog $rtlfiles
     fi
-    vlib work
-    vlog ${rtlfiles}
-    
     
 elif [ "$1" == "rv32ui" ] || [ "$1" == "rv32mi" ] || [ "$1" == "extra" ] || [ "$1" == "all" ] || [ "$1" == "" ] # regression tests
 then
@@ -108,7 +113,7 @@ then
             
             ####################################### EXTRACT TEXT AND DATA SECTIONS FROM BIN FILE #######################################
             printf "\textracting text and data sections....."
-            if (( $(python sections.py ${ONAME}.bin ${MEMORY}| grep DONE -c) != 0 )) # extract text section and data section from bin file
+            if (( $(python3 sections.py ${ONAME}.bin ${MEMORY}| grep DONE -c) != 0 )) # extract text section and data section from bin file
             then
                 printf "DONE!\n"
             else
@@ -118,24 +123,42 @@ then
            
            
             ################################################### TESTBENCH SIMULATION ###################################################
-            printf "\tsimulating with Modelsim....."
-            
-            if [ -d "./work/" ]  # check first if work library exists
-            then
-                vdel -all -lib work # delete old library folder
-            fi
-            vlib work
-            if (( $(grep "exception" -c <<< $testfile) != 0 )) # if current testfile name has word "exception" then that testfile will not halt on ebreak/ecall
-            then
-                vlog -quiet +define+HALT_ON_ILLEGAL_INSTRUCTION ${rtlfiles} # current testfile will halt on illegal instruction only
-            elif (( $(grep "sbreak" -c <<< $testfile) != 0 )) # if current testfile name has word "sbreak" then that testfile will halt only on ecall
-            then
-                vlog -quiet +define+HALT_ON_ECALL ${rtlfiles} # halt core on ecall
+            if [ $(command -v vlog) ] 
+            then                
+                printf "\tsimulating with Modelsim....."
+                
+                if [ -d "./work/" ]  # check first if work library exists
+                then
+                    vdel -all -lib work # delete old library folder
+                fi
+                vlib work
+                if (( $(grep "exception" -c <<< $testfile) != 0 )) # if current testfile name has word "exception" then that testfile will not halt on ebreak/ecall
+                then
+                    vlog -quiet +define+HALT_ON_ILLEGAL_INSTRUCTION ${rtlfiles} # current testfile will halt on illegal instruction only
+                elif (( $(grep "sbreak" -c <<< $testfile) != 0 )) # if current testfile name has word "sbreak" then that testfile will halt only on ecall
+                then
+                    vlog -quiet +define+HALT_ON_ECALL ${rtlfiles} # halt core on ecall
+                else
+                    vlog -quiet ${rtlfiles} # current testfile will halt on both ebreak/ecall 
+                fi
+                
+                a=$(vsim -quiet -batch -G MEMORY="${MEMORY}" rv32i_soc_TB -do "run -all;exit" | grep "PASS:\|FAIL:\|UNKNOWN:" -A1)
             else
-                vlog -quiet ${rtlfiles} # current testfile will halt on both ebreak/ecall 
+                printf "\tsimulating with Icarus Verilog....."
+                rm -f testbench.vvp # remove previous occurence of vvp file  
+
+                if (( $(grep "exception" -c <<< $testfile) != 0 )) # if current testfile name has word "exception" then that testfile will not halt on ebreak/ecall
+                then
+                    iverilog -o testbench.vvp -DHALT_ON_ILLEGAL_INSTRUCTION $rtlfiles # current testfile will halt on illegal instruction only
+                elif (( $(grep "sbreak" -c <<< $testfile) != 0 )) # if current testfile name has word "sbreak" then that testfile will halt only on ecall
+                then
+                    iverilog -o testbench.vvp -DHALT_ON_ECALL $rtlfiles # halt core on ecall
+                else
+                    iverilog -o testbench.vvp $rtlfiles
+                fi
+                a=$(vvp -n testbench.vvp | grep "PASS:\|FAIL:\|UNKNOWN:" -A1)
             fi
-            
-            a=$(vsim -quiet -batch -G MEMORY="${MEMORY}" rv32i_soc_TB -do "run -all;exit" | grep "PASS:\|FAIL:\|UNKNOWN:" -A1)
+
             if (( $(grep "PASS:" -c <<< $a) != 0 ))
             then
                 status=$(tail -n 1 <<< $a)
@@ -227,7 +250,7 @@ else    # DEBUG MODE: first argument given is the assembly file to be tested and
         
         ####################################### EXTRACT TEXT AND DATA SECTIONS FROM BIN FILE #######################################
         printf "\textracting text and data sections....."
-        if (( $(python sections.py ${ONAME}.bin ${MEMORY} | grep DONE -c) != 0 )) # extract text section and data section from bin file
+        if (( $(python3 sections.py ${ONAME}.bin ${MEMORY} | grep DONE -c) != 0 )) # extract text section and data section from bin file
         then
             printf "DONE!\n"
         else
@@ -240,25 +263,48 @@ else    # DEBUG MODE: first argument given is the assembly file to be tested and
        
        
         ################################################### TESTBENCH SIMULATION ###################################################
-        printf "\tsimulating with Modelsim.....\n"
-        printf "\n##############################################################\n"
-        if [ -d "./work/" ]  # check first if work library exists
+        if [ $(command -v vlog) ]
         then
-            vdel -all -lib work # delete old library folder
-        fi
-        vlib work
-        
-        if (( $(grep "exception" -c <<< $1) != 0 ))
-        then
-            vlog -quiet +define+HALT_ON_ILLEGAL_INSTRUCTION ${rtlfiles} # current testfile will halt on illegal instruction only      
-        elif (( $(grep "sbreak" -c <<< $testfile) != 0 )) # if current testfile name has word "sbreak" then that testfile will halt only on ecall
-        then
-            vlog -quiet +define+HALT_ON_ECALL ${rtlfiles} # halt core on ecall
+            printf "\tsimulating with Modelsim.....\n"
+            printf "\n##############################################################\n"
+            if [ -d "./work/" ]  # check first if work library exists
+            then
+                vdel -all -lib work # delete old library folder
+            fi
+            vlib work
+            
+            if (( $(grep "exception" -c <<< $1) != 0 ))
+            then
+                vlog -quiet +define+HALT_ON_ILLEGAL_INSTRUCTION ${rtlfiles} # current testfile will halt on illegal instruction only      
+            elif (( $(grep "sbreak" -c <<< $1) != 0 )) # if current testfile name has word "sbreak" then that testfile will halt only on ecall
+            then
+                vlog -quiet +define+HALT_ON_ECALL ${rtlfiles} # halt core on ecall
+            else
+                vlog -quiet ${rtlfiles} # current testfile will halt on both ebreak/ecall 
+            fi
+            vsim $2 -G MEMORY="${MEMORY}" rv32i_soc_TB -do "do wave.do;run -all"
+            a=$(vsim -batch -G MEMORY="${MEMORY}" rv32i_soc_TB -do "run -all;exit" | grep "PASS:\|FAIL:\|UNKNOWN:" -A1)
         else
-            vlog -quiet ${rtlfiles} # current testfile will halt on both ebreak/ecall 
+            printf "\tsimulating with Icarus Verilog.....\n"
+            printf "\n##############################################################\n"
+            rm -f ./testbench.vvp 
+
+            if (( $(grep "exception" -c <<< $1) != 0 ))
+            then
+                iverilog -o testbench.vvp -DHALT_ON_ILLEGAL_INSTRUCTION $rtlfiles # current testfile will halt on illegal instruction only
+            elif (( $(grep "sbreak" -c <<< $1) != 0 )) # if current testfile name has word "sbreak" then that testfile will halt only on ecall
+            then
+                iverilog -o testbench.vvp -DHALT_ON_ECALL $rtlfiles # halt core on ecall
+            else
+                iverilog -o testbench.vvp $rtlfiles # current testfile will halt on both ebreak/ecall 
+            fi
+            vvp -n testbench.vvp
+            if [ "$2" == "-gui" ]
+            then
+                gtkwave wave.gtkw
+            fi
+            a=$(vvp -n testbench.vvp | grep "PASS:\|FAIL:\|UNKNOWN:" -A1)
         fi
-        vsim $2 -G MEMORY="${MEMORY}" rv32i_soc_TB -do "do wave.do;run -all"
-        a=$(vsim -batch -G MEMORY="${MEMORY}" rv32i_soc_TB -do "run -all;exit" | grep "PASS:\|FAIL:\|UNKNOWN:" -A1)
         printf "##############################################################\n"
         if (( $(grep "PASS:" -c <<< $a) != 0 ))
         then    
