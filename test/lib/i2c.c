4e3ac5b55d2a61b525046fbd3acb4023a9c950ea
Angelo Jacobo <jacobo.angeloECE@gmail.com>
2023-03-25 17:37:14 +0800

    removed delay functions (moved to clint.c)

--- test/lib/i2c.c
+++ test/lib/i2c.c
@@ -7,18 +7,6 @@ volatile uint32_t *i2c_busy = (volatile uint32_t *) I2C_BUSY;
 volatile uint32_t *i2c_halt = (volatile uint32_t *) I2C_STOP;
 volatile uint32_t *i2c_ack = (volatile uint32_t *) I2C_ACK;
 
-// delay function uses MTIME register
-void  delay_ms(uint64_t ms) {
-	uint64_t initial_time = mtime_get_time();
-	while ((initial_time + ms) > (uint64_t)mtime_get_time()){ //do nothing while delay has not yet passed
-	}
-}
-
-// delay function using clock tick
-void  delay(uint32_t ticks) {
-	while(ticks!=0) --ticks; //stay here until ticks become zero
-}
-
 // start i2c by writing slave address (returns slave ack)
 uint8_t i2c_write_address(uint8_t addr){
     uint8_t ack;
@@ -35,7 +23,7 @@ void i2c_stop(void){
     *i2c_halt = 0x01;
     while(*i2c_busy);
     *i2c_halt = 0x00; //set it back to zero in preparation for next transaction
-     delay(100);
+    delay_ticks(100);
 }
 
 uint8_t i2c_write_byte(uint8_t data){
@@ -46,5 +34,4 @@ uint8_t i2c_write_byte(uint8_t data){
     ack = *i2c_ack; //check if slave acknowledged
     return ack;
 }
-
 
