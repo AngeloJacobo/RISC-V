ef15923faf328684e9a1c833606d7d5de671a787
AngeloJacobo <jacobo.angeloECE@gmail.com>
2023-04-04 16:15:47 +0800

    removed while loop to pass test

--- test/extra/test_gpio.c
+++ test/extra/test_gpio.c
@@ -2,13 +2,14 @@
 #include <rv32i.h>
 
 int main(){
+    int counter = 0;
     gpio_write_pin(10, 1); //write to a specific GPIO pin (automatically set pin to write mode)
     delay_ticks(100); //after 100 cpu clock ticks
     gpio_write_pin(10, 0); //write to a specific GPIO pin (automatically set pin to write mode)
     delay_ticks(100); //after 100 cpu clock ticks
     gpio_write_pin(10, 1); //write to a specific GPIO pin (automatically set pin to write mode)
     delay_ticks(100); //after 100 cpu clock ticks
-    while(1){
+    /*while(1)*/{
         toggle_gpio(5); //toggle a specific GPIO pin (automatically set pin to write mode)
         delay_ticks(100); //after 100 cpu clock ticks
     }
